718e4a586c107c5f363894b9f5779a2b5d398034
Kangmin <ehfehf0629@gmail.com>
2023-01-15 08:10:35 +0900

    fix: UserController

--- dokgo-die-server/src/main/java/com/example/dokgodieserver/domain/user/presentation/UserController.java
+++ dokgo-die-server/src/main/java/com/example/dokgodieserver/domain/user/presentation/UserController.java
@@ -1,10 +1,13 @@
 package com.example.dokgodieserver.domain.user.presentation;
 
+import com.example.dokgodieserver.domain.grade.service.GradeService;
+import com.example.dokgodieserver.domain.user.presentation.dto.request.GradeRequest;
 import com.example.dokgodieserver.domain.user.presentation.dto.request.SignInRequest;
 import com.example.dokgodieserver.domain.user.presentation.dto.request.SignUpRequest;
+import com.example.dokgodieserver.domain.conversation.presentation.dto.ConversationResponse;
+import com.example.dokgodieserver.domain.user.presentation.dto.response.MyPageResponse;
 import com.example.dokgodieserver.domain.user.presentation.dto.response.TokenResponse;
-import com.example.dokgodieserver.domain.user.service.UserSignInService;
-import com.example.dokgodieserver.domain.user.service.UserSignUpService;
+import com.example.dokgodieserver.domain.user.service.*;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.*;
@@ -18,6 +21,8 @@ public class UserController {
 
     private final UserSignUpService userSignUpService;
     private final UserSignInService userSignInService;
+    private final MyPageService myPageService;
+    private final GradeService gradeService;
 
     @ResponseStatus(HttpStatus.CREATED)
     @PostMapping
@@ -28,6 +33,17 @@ public class UserController {
     @PostMapping("/auth")
     public TokenResponse signIn(@RequestBody @Valid SignInRequest request) {
         return userSignInService.execute(request);
+    }
+
+    @GetMapping("/my-page")
+    public MyPageResponse getMyPage() {
+        return myPageService.execute();
+    }
+
+    @PutMapping("/grade/{user-id}")
+    public void grade(@PathVariable("user-id") Long userId,
+                      @RequestBody @Valid GradeRequest request) {
+        gradeService.execute(userId, request);
     }
 
 }
