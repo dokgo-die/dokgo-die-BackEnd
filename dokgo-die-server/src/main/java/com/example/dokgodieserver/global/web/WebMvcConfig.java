c9291bc19b7e8fff52e42374e1e32e56d9ae33aa
Kangmin <ehfehf0629@gmail.com>
2023-01-14 17:59:20 +0900

    add: WebMvcConfig

---
+++ dokgo-die-server/src/main/java/com/example/dokgodieserver/global/web/WebMvcConfig.java
@@ -0,0 +1,16 @@
+package com.example.dokgodieserver.global.web;
+
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+public class WebMvcConfig implements WebMvcConfigurer {
+
+    @Override
+    public void addCorsMappings(CorsRegistry registry) {
+        registry.addMapping("/**")
+                .allowedOrigins("*")
+                .allowedMethods("*")
+                .allowedHeaders("*");
+    }
+
+}
