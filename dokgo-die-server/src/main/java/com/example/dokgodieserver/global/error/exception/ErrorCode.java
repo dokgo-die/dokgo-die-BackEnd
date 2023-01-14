b5b391bfc3afaf18fa40efe538bed16c44842fdf
Kangmin <ehfehf0629@gmail.com>
2023-01-14 20:27:41 +0900

    add: S3 Exception

--- dokgo-die-server/src/main/java/com/example/dokgodieserver/global/error/exception/ErrorCode.java
+++ dokgo-die-server/src/main/java/com/example/dokgodieserver/global/error/exception/ErrorCode.java
@@ -7,12 +7,16 @@ import lombok.Getter;
 @AllArgsConstructor
 public enum ErrorCode {
 
+    SAVE_IMAGE_FAILED(400, "Save Image Failed"),
+
     EXPIRED_TOKEN(401, "Expired Token"),
     INVALID_TOKEN(401, "Invalid Token"),
 
+    PASSWORD_MIS_MATCH(401, "Password Mis Match"),
+
     //user
     USER_NOT_FOUND(404, "User Not Found"),
-    PASSWORD_MIS_MATCH(401, "Password Mis Match"),
+    IMAGE_NOT_FOUND(404, "Image Not Found"),
 
     INTERNAL_SERVER_ERROR(500, "Internal Server Error");
 
--- dokgo-die-server/src/main/java/com/example/dokgodieserver/infrastructure/s3/S3Facade.java
+++ dokgo-die-server/src/main/java/com/example/dokgodieserver/infrastructure/s3/S3Facade.java
@@ -4,6 +4,8 @@ import com.amazonaws.services.s3.AmazonS3Client;
 import com.amazonaws.services.s3.model.CannedAccessControlList;
 import com.amazonaws.services.s3.model.ObjectMetadata;
 import com.amazonaws.services.s3.model.PutObjectRequest;
+import com.example.dokgodieserver.global.excpetion.ImageNotFoundException;
+import com.example.dokgodieserver.global.excpetion.SaveImageFailedException;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Component;
 import org.springframework.web.multipart.MultipartFile;
@@ -19,7 +21,7 @@ public class S3Facade {
 
     public String uploadImage(MultipartFile image) {
         if (image.isEmpty()) {
-            throw ImageValueNotFoundException.EXCEPTION;
+            throw ImageNotFoundException.EXCEPTION;
         }
 
         String fileName = s3Properties.getBucket() + UUID.randomUUID() + image.getOriginalFilename();
