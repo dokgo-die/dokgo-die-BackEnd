1cc08d854ca1a192ee663218d4482889e5faccca
Kangmin <ehfehf0629@gmail.com>
2023-01-15 05:23:04 +0900

    fix

--- dokgo-die-server/src/main/java/com/example/dokgodieserver/domain/reels/domain/repository/ReelsRepository.java
+++
@@ -1,7 +0,0 @@
-package com.example.dokgodieserver.domain.reels.domain.repository;
-
-import com.example.dokgodieserver.domain.reels.domain.Reels;
-import org.springframework.data.repository.CrudRepository;
-
-public interface ReelsRepository extends CrudRepository<Reels, Long> {
-}
