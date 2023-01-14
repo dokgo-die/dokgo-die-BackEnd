5c9d229f0ceb532fb568b151a164c19cdccd7849
Kangmin <ehfehf0629@gmail.com>
2023-01-14 21:09:43 +0900

    add: ReelsRepository

---
+++ dokgo-die-server/src/main/java/com/example/dokgodieserver/domain/reels/domain/repository/ReelsRepository.java
@@ -0,0 +1,7 @@
+package com.example.dokgodieserver.domain.reels.domain.repository;
+
+import com.example.dokgodieserver.domain.reels.domain.Reels;
+import org.springframework.data.repository.CrudRepository;
+
+public interface ReelsRepository extends CrudRepository<Reels, Long> {
+}
