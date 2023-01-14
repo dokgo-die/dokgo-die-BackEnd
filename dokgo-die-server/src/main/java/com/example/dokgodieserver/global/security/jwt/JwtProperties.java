0858d7494be911ee4c48871be3a2f23650794849
Kangmin <ehfehf0629@gmail.com>
2023-01-14 19:37:24 +0900

    fix

--- dokgo-die-server/src/main/java/com/example/dokgodieserver/global/security/jwt/JwtProperties.java
+++ dokgo-die-server/src/main/java/com/example/dokgodieserver/global/security/jwt/JwtProperties.java
@@ -3,7 +3,7 @@ package com.example.dokgodieserver.global.security.jwt;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
 import org.springframework.boot.context.properties.ConfigurationProperties;
-import org.springframework.boot.context.properties.bind.ConstructorBinding;
+import org.springframework.boot.context.properties.ConstructorBinding;
 
 @Getter
 @AllArgsConstructor
