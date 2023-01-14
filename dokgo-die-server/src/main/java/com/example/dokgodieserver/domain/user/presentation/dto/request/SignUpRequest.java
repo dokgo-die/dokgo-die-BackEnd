dbf079f3a671805f4c8df39f3b2df05f81d385ae
Kangmin <ehfehf0629@gmail.com>
2023-01-15 06:01:26 +0900

    add: UserRequest

--- dokgo-die-server/src/main/java/com/example/dokgodieserver/domain/user/presentation/dto/request/SignUpRequest.java
+++ dokgo-die-server/src/main/java/com/example/dokgodieserver/domain/user/presentation/dto/request/SignUpRequest.java
@@ -19,4 +19,5 @@ public class SignUpRequest {
     @NotBlank
     @Pattern(regexp = "(?=.*[a-z])(?=.*[0-9])(?=.*[~!@#$%^&*()_+-=?/])[a-zA-Z0-9~!@#$%^&*()_+-=?/]{8,20}$", message = "비밀번호는는 8자에서 20자 사이입니다.")
     private String password;
+
 }
